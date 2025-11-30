67eb43933458cba17e3498b04a7ad7bd9320b716
Joel Andedi <joellucasandedi@gmail.com>
2025-12-01 00:33:32 +0100

    Ajout duu nom de domaine en prod pour le cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,13 @@ import cors from 'cors';
 import { env } from './env';
 
 export const corsMiddleware = cors({
-  origin: env.corsOrigin,
+  origin: (origin, callback) => {
+    const allowedOrigins = env.corsOrigin.split(',').map(o => o.trim());
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 });
